c4ac3d6f5104c81679611b38dc472bf36e0049d3
Rony <ronnjhost@gmail.com>
2026-05-06 22:56:14 -0500

    first commit

--- app/src/main/java/com/example/saborandino/data/model/Cliente.kt
+++
@@ -1,6 +0,0 @@
-package com.example.saborandino.data.model
-
-data class Cliente(
-    val nombre: String,
-    val correo: String
-)
--- app/src/main/java/com/example/saborandino/data/model/ItemPedido.kt
+++
@@ -1,6 +0,0 @@
-package com.example.saborandino.data.model
-
-data class ItemPedido(
-    val plato: Plato,
-    val cantidad: Int
-)
